15658f70d743cf6dd31770687df414afb1e63ce3
lindaheriksson <linda.h.eriksson@gmail.com>
2015-08-06 10:59:51 +0200

    quiz page using text data

--- quiz/views.py
+++ quiz/views.py
@@ -1,10 +1,19 @@
+# coding: utf-8 
+
 from django.shortcuts import render
 
 def index(request):
-	return render(request, "quiz/index.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/index.html", context)
 
-def quiz(request):
-	return render(request, "quiz/quiz.html")
+def quiz(request, slug):
+	context = {
+		"quiz": quizzes[slug],
+		"quiz_slug": slug,
+	}
+	return render(request, "quiz/quiz.html", context)
 
 def question(request):
 	return render(request, "quiz/question.html")
@@ -14,7 +23,7 @@ def result(request):
 
 quizzes = {
 	"klassiker": {
-   		"name": "Klassiska böcker",
+   		"name": u"Klassiska böcker",
 	   	"description": "Hur bra kan du dina klassiker?"
 	},
 	"fotboll": {
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from quiz import views
 urlpatterns = [
 	url(r"^$", views.index),
-	url(r"^quiz/[a-z-]+/$", views.quiz),
+	url(r"^quiz/([a-z-]+)/$", views.quiz),
 	url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
 	url(r"^quiz/[a-z-]+/result/$", views.result),
 ]
