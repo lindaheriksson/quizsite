13359d1e3f6ad543fcfe34228ffd1b4c6963bcaf
lindaheriksson <linda.h.eriksson@gmail.com>
2015-08-06 11:15:20 +0200

    data on question page

--- quiz/views.py
+++ quiz/views.py
@@ -15,8 +15,16 @@ def quiz(request, slug):
 	}
 	return render(request, "quiz/quiz.html", context)
 
-def question(request):
-	return render(request, "quiz/question.html")
+def question(request, slug, number):
+	context = {
+		"question_number": number,
+		"question": u"Hur många bultar har Ölandsbron?",
+		"answer1": u"12",
+		"answer2": u"66 400",
+		"answer3": u"7 428 954",
+		"quiz_slug": slug,
+	}
+	return render(request, "quiz/question.html", context)
 
 def result(request):
 	return render(request, "quiz/result.html")
