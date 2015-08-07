8d0bd9bec33f57624a5a39c1e0c20c03201bf387
lindaheriksson <linda.h.eriksson@gmail.com>
2015-08-07 12:10:25 +0200

    soon push

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -105,3 +105,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATIC_ROOT = 'staticfiles'
